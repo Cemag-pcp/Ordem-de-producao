fb8719b0f5c1186f39b1e957ad2621e8c5089e93
luan <luan640@gmail.com>
2023-12-29 13:32:49 -0300

    formato data

--- ordem_de_producao.py
+++ ordem_de_producao.py
@@ -557,8 +557,10 @@ if submit_button:
                 # sh.values_append('Pintura', {'valueInputOption': 'RAW'}, {
                 #                  'values': tab_completa1})
         
-        
+        tab_completa['Datas'] = tab_completa['Datas'].apply(lambda x: datetime.strptime(x,'%Y-%d-%m').strftime('%Y-%m-%d'))
+
         data_insert_sql = tab_completa[['Célula','Código','Peca','cor','Qtde_total','Datas']]
+
         data_insert_sql = data_insert_sql.values.tolist()
 
         insert_pintura(datetime.strptime(tipo_filtro,'%d/%m/%Y').strftime('%Y-%m-%d'), data_insert_sql)
